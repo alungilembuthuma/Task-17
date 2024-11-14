14c00185a34241ece69891d6f20b6bccb8d7b239
alungilembuthuma <alungilembuthuma0320@gmail.com>
2024-11-14 15:00:50 +0200

    deployment

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-// server.js
-
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Correct way to use post with a valid callback
 app.post('/create', (req, res) => {
   res.send('Data created successfully!');
 });
 
-// Ensure the handler function is correctly passed
+app.get('/', (req, res) => {
+    res.send('Welcome to my Express server!');
+  });
+  
 const myPostHandler = (req, res) => {
   res.send('Data created successfully!');
 };
 app.post('/create', myPostHandler); // This also works fine.
 
 app.listen(3000, () => {
-  console.log('Server is running on port 5000');
+  console.log('Server is running on port 3000');
 });
